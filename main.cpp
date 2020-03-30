42d1607a551f3d0a16c21490e38fbca3f7216e72
Joao Stephan <joaossmauricio@gmail.com>
2020-03-30 17:29:04 -0300

    Tutorial 01 feito

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <string.h>
 #include "Base.h"
@@ -9,5 +9,46 @@ using namespace std;
 
 int main()
 {
+    inicializar(530, 800);
 
+    criarInstancia();
+    criarAnimacao(100, 0, "texturas/raquete.png");
+    criarInstancia();
+    criarAnimacao(100, 1, "texturas/raquete.png");
+    criarInstancia();
+    criarAnimacao(100, 2, "texturas/bola.png");
+    float velocidadeX = 1.5, velocidadeY = 1.5;
+
+    colocarInstanciaPosicao(0, 30 , 150);
+    colocarInstanciaPosicao(1, 770, 500);
+    colocarInstanciaPosicao(2, 400, 300);
+
+    //GAME LOOP
+    while(janelaAberta())
+    {
+        //MOVIMENTACAO JOGADOR
+        if(pressionarTecla(TECLA_CIMA))
+            moverInstancia(0, 0, -1.2);
+        if(pressionarTecla(TECLA_BAIXO))
+            moverInstancia(0, 0, 1.2);
+
+        //MOVIMENTACAO ADVERSARIO
+        if(retornaPosicaoInstanciaY(2) > retornaPosicaoInstanciaY(1))
+            moverInstancia(1, 0, 1.2);
+        else
+            moverInstancia(1, 0, -1.2);
+
+        //MOVIMENTACAO BOLA
+        if(retornaPosicaoInstanciaX(2) > 800 || retornaPosicaoInstanciaX(2) < 0)
+            velocidadeX *= -1;
+        if(retornaPosicaoInstanciaY(2) > 530 || retornaPosicaoInstanciaY(2) < 0)
+            velocidadeY *= -1;
+        if(analisaColisaoInstancias(0, 2) || analisaColisaoInstancias(1, 2))
+            velocidadeX *= -1;
+        moverInstancia(2, velocidadeX, velocidadeY);
+
+        atualiza();
+    }
+
+    return 0;
 }
