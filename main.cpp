de0b3d4b99e2936417bde1da40ff6c4b2f32d524
Joao Stephan <joaossmauricio@gmail.com>
2020-03-30 19:43:37 -0300

    Instancias do jogo ja criadas

--- main.cpp
+++ main.cpp
@@ -9,5 +9,37 @@ using namespace std;
 
 int main()
 {
+    inicializar();
 
+    criarInstancia();
+    colocarEscalaInstancia(0, 0.5);
+    criarAnimacao(100, 0, "texturas/nave.png");
+
+    int inimigos[20];
+    for(int i = 0; i < 20; i++)
+    {
+        inimigos[i] = criarInstancia();
+        colocarEscalaInstancia(inimigos[i], 0.5);
+        virarSpriteYInstancia(inimigos[i]);
+        colocarInstanciaPosicao(inimigos[i], -50, -50);
+        criarAnimacao(100, inimigos[i], "texturas/nave.png");
+    }
+
+    int tiros[20];
+    for(int i = 0; i < 20; i++)
+    {
+        tiros[i] = criarInstancia();
+        colocarEscalaInstancia(tiros[i], 0.2);
+        colocarInstanciaPosicao(tiros[i], -50, -50);
+        criarAnimacao(100, tiros[i], "texturas/tiro.png");
+    }
+
+    //GAME LOOP
+    while(janelaAberta())
+    {
+
+        atualiza();
+    }
+
+    return 0;
 }
